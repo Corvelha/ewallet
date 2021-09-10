64395a7f9629e5ea3d2ca2d05439a7d1f5b0fd39
Brenda Nicolli <kpopb612yaoi@gmail.com>
2021-09-10 16:55:56 -0300

    feat: quase acabando

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 //transações fictícias cadastradas(criadas manualmente)
 //pegando um item cadastrado/ salvo no bancos de dados local
-var transactions = 
-JSON.parse(localStorage.getItem("@ewallet/transactions")) || [];
-
+var transactions =
+  JSON.parse(localStorage.getItem("@ewallet/transactions")) || [];
 
 //adiciona o corpo da tabela na variavel table
 var table = document.querySelector("table tbody");
@@ -53,11 +52,12 @@ closeBtn.addEventListener("click", () => {
   form.reset();
 });
 
-form.addEventListener("submit", (event)=> {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   var data = new FormData(event.target);
-  var{title, currency, identifier, price, category} = Object.fromEntries(data);
+  var { title, currency, identifier, price, category } =
+    Object.fromEntries(data);
 
   //Pegar a data atual no formato Dia(DD), Mês(MM), Ano(YYYY)
   var date = new Date().toLocaleDateString();
@@ -70,16 +70,43 @@ form.addEventListener("submit", (event)=> {
     identifier,
     //determina o tamanho da sua rede de objetos(abaixo)
     id: transactions.length + 1,
-    date
-  }
+    date,
+  };
   //empurrar todas as novas transações junto com as anteriores
-  transactions.push(transaction)
+  transactions.push(transaction);
 
-  //usando uma função do JSON para transformar em string 
-  localStorage.setItem("@ewallet/transactions", JSON.stringify (transactions));
+  //usando uma função do JSON para transformar em string
+  localStorage.setItem("@ewallet/transactions", JSON.stringify(transactions));
   //está gravando no banco de dados local um item
   window.location.reload(); //vai recarrecar a página sozinho após atualizar a página
 });
+
+var entrada = document.querySelector(".in h1");
+var saída = document.querySelector(".out h1");
+var total = document.querySelector(".total h1");
+
+var valoresEntrada = transactions.reduce((count, currentValue) => {
+  //estrutura condicional(if)
+  if (currentValue.category === "entrada") {
+    return count + currentValue.price;
+  } else {
+    return count;
+  }
+}, 0);
+var valoresSaída = transactions.reduce((count, currentValue) => {
+  //estrutura condicional(if)
+  if (currentValue.category === "saída") {
+    return count + currentValue.price;
+  } else {
+    return count;
+  }
+}, 0);
+
+var somatório = valoresEntrada - valoresSaída;
+
+total.innerHTML = moneyFormat("BRL", somatório);
+saída.innerHTML = moneyFormat("BRL", valoresSaída);
+entrada.innerHTML = moneyFormat("BRL", valoresEntrada);
 
 //Métodos ou funções
 function moneyFormat(currency, price) {
@@ -88,4 +115,4 @@ function moneyFormat(currency, price) {
     currency: currency,
   }).format(price);
   return value;
-};
+}
